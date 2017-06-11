2b08dbdd786bfda8c125a9a62fd394cb32f8631a
benosg <benosg@gmail.com>
2017-06-11 00:19:10 -0400

    V3

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 Solemne 2 Taller de Construccion de software
 IP CIISA
 profesor Claudio Castillo.
-Desarrollo en php que al ingresar una tarea se agrege en una lista en la pagina, desarollado con patron mvc y clases en php.
-html5
-bootstrap
+Desarrollo en php que al ingresar una tarea se agrege en una lista en la pagina, desarollado con patron mvc y clases en php
+
+html5,bootstrap
 
 Desarrollado por Benito Serrano y Marco Sepulveda
--- index.php
+++ index.php
@@ -23,23 +23,38 @@ session_start();
                 </div>
         </div>
         </form>
-        <?php
+    </div>
+    <div class="container">
+        <div class="form-group">
+            <table class="table table-condensed table-striped">
+                <?php
         if (isset($_SESSION['tareas'])){
             foreach (array_keys($_SESSION['tareas']) as $i){
                 ?>
-            <form action="controller.php" method="post">
-                <div aling="left" class="form-group form-inline">
-                    <input type="hidden" name="action" value="2" />
-                    <button type="submit" class="btn btn-success"> x </button>
-                    <?php echo $_SESSION['tareas'][$i]; ?>
-                    <input type="hidden" name="id" value="<?php echo $i; ?>" />
-                </div>
-            </form>
-            <?php
+                    <form action="controller.php" method="post">
+                        <div aling="left" class="form-group form-inline">
+
+                            <tr>
+                                <td>
+                                    <input type="hidden" name="action" value="2" />
+                                    <button type="submit" class="btn btn-success"> x </button>
+                                </td>
+                                <td>
+                                    <?php echo $_SESSION['tareas'][$i]; ?>
+                                    <input type="hidden" name="id" value="<?php echo $i; ?>" />
+                                </td>
+                            </tr>
+
+                        </div>
+                    </form>
+                    <?php
             }
         }
             ?>
+        </div>
     </div>
+    </table>
+
 </body>
 
 </html>
